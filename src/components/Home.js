e67b9686a115d536bb1fb595ef313655718f1ebe
GoktugYmsk <gogtug033@outlook.com>
2023-03-31 12:46:22 +0300

    The data from the input is printed to the screen in the other component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { dark } from './altcomponents/Child1'
-import { light } from './altcomponents/Child1'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { dark, light } from "./altcomponents/Child1";
 
 function Home() {
-    const dispatch = useDispatch()
-    const screen = useSelector((state) => state.activeTheme.theme)
+  const dispatch = useDispatch();
+  const screen = useSelector((state) => state.activeTheme.theme);
 
-    return (
-        <div className='home'>
-            Home sayfası
-            <button onClick={() => dispatch(dark())} >Koyu tema</button>
-            <button onClick={() => dispatch(light())} >Açık tema</button>
-            <br />
-            <br />
-            {!screen ? 'siyah ekran' : 'beyaz ekran'}
-            <br />
-        </div>
-    )
+  return (
+    <div className={`homeContainer ${!screen ? 'homeDark' : 'homeLight'}`}>
+      Home sayfası
+      <button onClick={() => dispatch(dark())}>Koyu tema</button>
+      <button onClick={() => dispatch(light())}>Açık tema</button>
+      <br />
+      <br />
+      {!screen ? "siyah ekran" : "beyaz ekran"}
+      <br />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
